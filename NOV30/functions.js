ea2f8ba4037af78dd9af81418f671ef9a621851d
Grant Wilhelm <grant.c.wilhelm@gmail.com>
2018-11-30 14:51:42 -0700

    newsies, sematic and wedding docs update

---
+++ NOV30/functions.js
@@ -0,0 +1,22 @@
+// let calculatorIsOn = true;
+// function pressPowerButton  () {
+//     if (calculatorIsOn) {
+//       console.log('Calculator turning off.');
+//       calculatorIsOn = false;
+//     } else {
+//       console.log('Calculator turning on.');
+//       calculatorIsOn = true;
+//     }
+//       };
+
+//       pressPowerButton();
+//   // Output: Calculator turning on.
+
+//       pressPowerButton();
+//       // Output: Calculator turning off.
+
+
+function scope2 (){
+    var x = 2
+}]
+console.log(x);
--- terminal-practice/dir-one/notes.js
+++
@@ -1,6 +0,0 @@
-// // Conditionals; there are IF and Switch statements
-// if (){
-//     //this is the set up for an if statement; () followed by a {
-
-//    // }
-// }
