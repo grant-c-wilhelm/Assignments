f60fbd7d68eb4de61dc9508a48c123ca39f6ae21
Grant Wilhelm <grant.c.wilhelm@gmail.com>
2018-12-03 15:02:43 -0700

    newsies submission

--- NOV30/functions.js
+++ NOV30/functions.js
@@ -15,8 +15,26 @@
 //       pressPowerButton();
 //       // Output: Calculator turning off.
 
+//to use a function is a 'stored procedure' that takes input and performs a taks/outputs a value
+//to use a funtion it first must be DECLARED or DEFINED as either an expression or statement:
+//you can have DECLARATIONS and EXPRESSIONS...EXPRESSIONS are special and violate the usual rules of reading
+//from left to right and instead it looks for those functions before running the code so they can be anywhere 
+//in your code
 
-function scope2 (){
-    var x = 2
-}]
-console.log(x);
+//execute a function using () syntax
+
+//ther is no sppon
+var pen = {
+    magicInk: {
+    getColor: function () {
+        var colors = ['red', 'white', 'green', 'maroon'];
+        var randomIndex = Math.floor(Math.random() * colors.length);
+        return colors [randomIndex];
+    }
+
+    price= 2,
+    isGelPen: false,
+    write: function() {
+        console.log("I'm a " + this.color + " pen that costs $" + this.price);
+    }
+}
