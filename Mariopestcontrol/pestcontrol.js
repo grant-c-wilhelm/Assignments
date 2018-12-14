3b16ed1a32beb4810b48e848ab25d9e59e3b8d5f
V School <learn@vschool.io>
2018-12-14 16:02:35 -0700

    feedback

--- Mariopestcontrol/pestcontrol.js
+++ Mariopestcontrol/pestcontrol.js
@@ -3,6 +3,8 @@ addButton.addEventListener('click',function(){
     var num1 = document.querySelector('.one').value;   //refs 1st input box
     var num2 = document.querySelector('.two').value;   //refs 2nd input box
     var num3 = document.querySelector('.three').value;   //refs 2nd input box
+
+    //instead of an alert, print the result into an actual HTML element, such as a <p>
     alert(parseInt(num1)*5 + parseInt(num2)*7 + parseInt(num3)*11);
 })
 
