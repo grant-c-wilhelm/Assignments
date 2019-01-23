a931b04095209432ffca18584b579cf4c0436630
Grant Wilhelm <grant.c.wilhelm@gmail.com>
2019-01-23 08:09:02 -0700

    server creation code

--- 1react/Server/database.js
+++ 1react/Server/database.js
@@ -12,7 +12,7 @@ const Database = function(){
     this.cosmicBeverages = []
 }
 Database.prototype.find = function(){
-    return this.cosmicBeverages
+    return this.cosmicBeverages;
 }
 
 Database.prototype.save = function (beverage){
@@ -20,7 +20,32 @@ Database.prototype.save = function (beverage){
     this.cosmicBeverages.push(newBeverage);
     return newBeverage;
 }
+Database.prototype.findByIdAndRemove = function (id){
+    //find beverage with the matching ID
+    const foundBeverage = this.cosmicBeverages.find(beverage => {
+        return beverage._id === id;
+    })
+    if(foundBeverage === undefined) return;
 
+    //find the inex of the given beverage
+    const index = this.cosmicBeverages.indexOf(foundBeverage);
+    
+    //remove it from teh array
+    this.cosmicBeverages.splice(index, 1);
+}
+Database.prototype.findByAndUpdate = function (id ,updates){
+    const foundBeverage = this.cosmicBeverages.find(beverage =>{
+        return beverage._id === id;
+    })
+    if(foundBeverage === undefined) return;
 
+    const index = this.cosmicBeverages.indexOf(foundBeverage); 
+    const newBeverage = { ...foundBeverage, ...updates};
+
+    this.cosmicBeverages.splice(index, 1, newBeverage);
+
+    return newBeverage;
+
+ }
 
 module.exports = new Database();
--- 1react/Server/server.js
+++ 1react/Server/server.js
@@ -6,21 +6,45 @@ const app = express();
 //Database
 
 const database = require('./database.js');
-//make sure to unclude this, it converts everything ot json
+
+//make sure to include this, it converts everything ot json
 app.use(express.json());
 
 
 app.get('/cosmicBeverages', (req, res) => {
-   const foundBeverages = database.find();
+    const foundBeverages = database.find();
     res.send(foundBeverages)
-    })
+    res.status(204).send()
+})
 app.post('/cosmicBeverages', (req, res) => {
     //see what the body is stating
     const beverageToSave = req.body;
     //add it to the database
     const savedBeverage = database.save(beverageToSave)
+
+    //send back a response containing the newly added items
     res.send(savedBeverage);
 })
 
+app.delete('/cosmicBeverage/:id', (req, res) => {
+    //grab the ID from the req
+    const id = req.params.id;
+    //find what to be deleted
 
-app.listen(process.env.PORT, () => console.log('server listening on Port 8080'))
+    //remove that thing
+    database.findByIdAndRemove(id);
+
+    //return back a confirm of removal
+    res.status(204).send()
+}) 
+ app.put('/cosmicBeverages/:id', (req, res) => {
+     const id = req.params.id;
+
+     const updater = req.body
+
+    const updateBeverage = database.findByIdAndRemove(id,update)
+
+
+ })
+
+app.listen(process.env.PORT, () => console.log(`server listening on Port ${process.env.PORT}`))
