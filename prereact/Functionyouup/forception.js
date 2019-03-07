474049679e08d367294aeb0a6fa1b604d08b0664
Grant Wilhelm <grant.c.wilhelm@gmail.com>
2019-03-06 17:34:47 -0700

    JS and React practice

--- prereact/Functionyouup/forception.js
+++ prereact/Functionyouup/forception.js
@@ -1,12 +1,37 @@
+// function forception(people, alphabet){
+//     var alphabet = "abcdefghijklmnopqrstuvwxyz"
+//     var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+//     var arr = [];
+//     for (i=0; i<people.length;i++){
+//         arr.push(people[i]+ ',' + alphabet);
+//     }console.log(arr);
+// }
+// forception();
 
+const arr = [1, 4, 5, 3, 4, 5, 6, 7];
 
-
-function forception(people, alphabet){
-    var alphabet = "abcdefghijklmnopqrstuvwxyz"
-    var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
-    var arr = [];
-    for (i=0; i<people.length;i++){
-        arr.push(people[i]+ ',' + alphabet);
-    }console.log(arr);
+// function includesArr (){
+//     if (arr.includes(10)){return true;}
+//     else {
+//         return false;
+//     }
+//     console.log(7**9)
+// }
+// console.log(includesArr());
+function User(fName, lName, age, favMovie, favSport) {
+  this.firstName = fName;
+  this.lastName = lName;
+  this.age = age;
+  this.favoriteMovie = favMovie;
+  this.favoriteSport = favSport;
 }
-forception();
+
+function forLoop() {
+  var objectArray = [];
+  for (var i = 0; i < 100; i++) {
+    var newObj = new User("Joe", "Schmoe", 42, "Ernest Goes to Camp", "UFC");
+    objectArray.push(newObj);
+  }
+  return objectArray;
+}
+console.log(forLoop());
--- prereact/ifOlympics/ifolympics.js
+++ prereact/ifOlympics/ifolympics.js
@@ -1,52 +1,67 @@
-if(5>3) {
-    console.log("yup, 5 is greater than 3!")
-};
+// if(5>3) {
+//     console.log("yup, 5 is greater than 3!")
+// };
 
-var cat = "cat";
-var three = 3;
-var dog = "dogs";
+// var cat = "cat";
+// var three = 3;
+// var dog = "dogs";
 
-if (cat.length == three) {
-    console.log('cat has 3 letters')
-};
+// if (cat.length == three) {
+//     console.log('cat has 3 letters')
+// };
 
-if (cat.length == 3 & dog.length==3){
-    console.log('They have the same number of letters')}
-    else {
-        console.log('Thats a negative mother goose!')
-    };
-var person = {
-  name: "Bobby",
-  age: 12
-}
-if (person.age>=13) {
-    console.log('you can go to the movies and see pg-13!!')
-}
-else if (person.age<=12){
-    console.log('Sorry little ' + person.name + ' you gotta stay home!')
-}
- if (person.name.startsWith('B')){
-    console.log('come on in!')
-};
+// if (cat.length == 3 & dog.length==3){
+//     console.log('They have the same number of letters')}
+//     else {
+//         console.log('Thats a negative mother goose!')
+//     };
+// var person = {
+//   name: "Bobby",
+//   age: 12
+// }
+// if (person.age>=13) {
+//     console.log('you can go to the movies and see pg-13!!')
+// }
+// else if (person.age<=12){
+//     console.log('Sorry little ' + person.name + ' you gotta stay home!')
+// }
+//  if (person.name.startsWith('B')){
+//     console.log('come on in!')
+// };
 
-if (person.name.startsWith('B') && person.age >=18){
-    console.log('Welcome to the show! Come inside!')} 
-    else {
-        console.log('hit the road bub!')
-    };
-if (cat.includes(dog)){
-    console.log('damn skippity it does')
-} else {
-    console.log('What in tarnation are you trying to say!?')
-}
-var thisTrue = true;
-if (typeof thisTrue ==='boolean'){
-    console.log('yup thats a boolean botha')
-}   else {
-    console.log('Definitley not a boolean bro')
-};
-if (typeof notDefined !=='undefined'){
-    console.log("yah man, thats not a dfined variable")
-}   else{
-    console.log('yup that is a defined variable somewhere in this code')
-};
+// if (person.name.startsWith('B') && person.age >=18){
+//     console.log('Welcome to the show! Come inside!')} 
+//     else {
+//         console.log('hit the road bub!')
+//     };
+// if (cat.includes(dog)){
+//     console.log('damn skippity it does')
+// } else {
+//     console.log('What in tarnation are you trying to say!?')
+// }
+// var thisTrue = true;
+// if (typeof thisTrue ==='boolean'){
+//     console.log('yup thats a boolean botha')
+// }   else {
+//     console.log('Definitley not a boolean bro')
+// };
+// if (typeof notDefined !=='undefined'){
+//     console.log("yah man, thats not a dfined variable")
+// }   else{
+//     console.log('yup that is a defined variable somewhere in this code')
+// };
+
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line;
+  
+  var playerNumber = 16;       // Change this Line
+  var player = testObj[playerNumber];   // Change this Line
+
+  console.log(playerNumber)
+  console.log(player)
